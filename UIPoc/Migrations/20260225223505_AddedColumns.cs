da98ac0c7497f8303e31c9e8576f2cb6d36b54a4
Mark Tsizis <tsizism@yahoo.ca>
2026-02-25 17:39:05 -0500

    AddedColumns

---
+++ UIPoc/Migrations/20260225223505_AddedColumns.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UIPooc.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Commission",
+                table: "Transactions",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "FlagMaxIndex",
+                table: "Holdings",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "FlagMinIndex",
+                table: "Holdings",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "LastTxnQuantity",
+                table: "Equities",
+                type: "decimal(18,4)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,4)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "LastTxnPrice",
+                table: "Equities",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "HoldingLow",
+                table: "Equities",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "HoldingHigh",
+                table: "Equities",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CurrentPrice",
+                table: "Equities",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "FlagMax",
+                table: "Equities",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "FlagMin",
+                table: "Equities",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "FlagMaxIndex",
+                table: "Holdings");
+
+            migrationBuilder.DropColumn(
+                name: "FlagMinIndex",
+                table: "Holdings");
+
+            migrationBuilder.DropColumn(
+                name: "FlagMax",
+                table: "Equities");
+
+            migrationBuilder.DropColumn(
+                name: "FlagMin",
+                table: "Equities");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Commission",
+                table: "Transactions",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "LastTxnQuantity",
+                table: "Equities",
+                type: "decimal(18,4)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,4)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "LastTxnPrice",
+                table: "Equities",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "HoldingLow",
+                table: "Equities",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "HoldingHigh",
+                table: "Equities",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CurrentPrice",
+                table: "Equities",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
