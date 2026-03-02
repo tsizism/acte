ba1210eade84e856a860e2dec4e61f96b4649fe4
Mark Tsizis <tsizism@yahoo.ca>
2026-03-01 20:02:17 -0500

    Added Equities page

---
+++ UIPoc/wwwroot/js/app.js
@@ -0,0 +1,8 @@
+window.downloadFile = function (fileName, dataUrl) {
+    const link = document.createElement('a');
+    link.href = dataUrl;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
