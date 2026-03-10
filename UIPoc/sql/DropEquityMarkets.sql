84938e9ce0eaa05c637d6da7838a448fe98a6967
Mark Tsizis <tsizism@yahoo.ca>
2026-03-09 20:43:02 -0400

    Drop some tables

---
+++ UIPoc/sql/DropEquityMarkets.sql
@@ -0,0 +1,17 @@
+USE [HoldingsDb]
+GO
+
+/****** Object: Table [dbo].[EquityMarkets] Script Date: 3/9/2026 8:40:24 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+-- DROP TABLE [dbo].[EquityMarkets];
+
+TRUNCATE TABLE [dbo].[EquityMarkets];
+GO
+
+
